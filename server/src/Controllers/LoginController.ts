f60bc73086f16b56c3ea191c5861e4c2ea998f45
mithesh16 <mitheshsrini@gmail.com>
2024-06-23 18:16:35 +0530

    JWT feature completed

--- server/src/Controllers/LoginController.ts
+++ server/src/Controllers/LoginController.ts
@@ -6,7 +6,11 @@ import { logindata,signupdata,otpdata } from '../Types/Types'
 import bcrypt from "bcryptjs";
 import * as nodemailer from 'nodemailer'
 import OTPModel from '../Models/OTPModel';
+import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
+export const SECRET_KEY: Secret = 'your-secret-key-here';
 
+const access_token_secret='76867f5af83866f9ea17938856b1b1fe9f07c8b1525d11f0172fc7d0eb6354d719aa9230e74ca6b8af1ae8eccf731b1cfd965ce4691fa0a1c7ed3ed50b9af451';
+const refresh_token_secret='2c2ad763e1d532c0438036549bbc8defe68e6aa0ab3be157479faaa2c5265870447a70dc32c4e0f4c0ee9e3ab1bfaa07b71afa9ea64c09e5c4bce25394a53876'
 
 const transporter = nodemailer.createTransport({
     port: 465,               // true for 465, false for other ports
@@ -27,8 +31,6 @@ export const signup=async(req:any,resp:any)=>{
     }
     else{
         await sendOTP(email);
-        // const hashedPassword = await bcrypt.hash(password, 10);
-        // const newUser=await User.create({firstname,lastname,email,password:hashedPassword})
         resp.status(200).json({msg:"OTPSENT"});
     }
 }
@@ -44,15 +46,17 @@ export const login=async(req:any,resp:any)=>{
     if(existingUser){
     const isMatch=await bcrypt.compare(password,existingUser.password)
     if(isMatch){
-        resp.status(200).json({msg:"LoginSuccessful",user:existingUser});
+        const token=jwt.sign({id:existingUser._id},access_token_secret)
+        existingUser.password=''
+        resp.status(200).json({msg:"LoginSuccessful",user:existingUser,token:token});
     }
     else{
         // const newUser=await User.create({firstname,lastname,email,password})
-        resp.status(400).json({msg:"Password Invalid"});
+        resp.status(200).json({msg:"Password Invalid"});
     }
 }
 else{
-    resp.status(400).json({msg:"Email Incorrect"});
+    resp.status(200).json({msg:"Email Not Registered"});
 }
 }
 catch(err:any){
@@ -76,7 +80,7 @@ const sendOTP=async(email:string)=>{
         expiresAt:Date.now()+300000}
     )
     const mailData = {
-        from: 'mithesh1606@gmail.com',  
+        from: 'mitheshsrini@gmail.com',  
           to: email,   
           subject: 'OTP Verification',
           text: 'Hello world',
@@ -100,7 +104,7 @@ const sendOTP=async(email:string)=>{
 export const verifyOTP=async(req:any,resp:any)=>{
     const {signupdata,otp}=req.body
     const{firstname,lastname,email,password}=signupdata
-    console.log(signupdata)
+
     const existingOTP=await OTPModel.findOne({user:signupdata.email})
     try{
     if(existingOTP){
@@ -109,8 +113,11 @@ export const verifyOTP=async(req:any,resp:any)=>{
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser=await User.create({firstname,lastname,email,password:hashedPassword})
        if(newUser){
-        //const deleteOTP=await OTPModel.findByIdAndDelete({id:existingOTP.id})
-        resp.status(200).json({msg:"OTPVERIFIED"});
+        const deleteOTP=await OTPModel.deleteOne({_id:existingOTP._id})
+        console.log(deleteOTP)
+        const token=jwt.sign({id:newUser._id},access_token_secret)
+        newUser.password=''
+        resp.status(200).json({msg:"OTPVERIFIED",user:newUser,token:token});
        }
        else{
         resp.status(400).json({msg:"RESIGNUP"});
@@ -118,7 +125,7 @@ export const verifyOTP=async(req:any,resp:any)=>{
     }
     else{
         // const newUser=await User.create({firstname,lastname,email,password})
-        resp.status(400).json({msg:"OTPINVALID"});
+        resp.status(200).json({msg:"OTPINVALID"});
     }
 }
 else{
@@ -128,4 +135,19 @@ else{
 catch(err:any){
 resp.status(400).json({msg:err.message})
 }
+}
+
+export const resendOTP=async(req:any,resp:any)=>{
+    try {
+    const {email}=req.body;
+    const existingOTP=await OTPModel.findOne({user:email})
+    if(existingOTP){
+    const deleteOTP=await OTPModel.deleteOne({_id:existingOTP._id})
+    }
+    await sendOTP(email);
+    resp.status(200).json({msg:"OTPSENT"});
+
+    } catch (error:any) {
+        resp.status(400).json({msg:error.message});
+    }
 }
--- server/src/Routes/LoginRoutes.ts
+++ server/src/Routes/LoginRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 
 const router=express.Router()
-import {signup,login,logout, verifyOTP} from '../Controllers/LoginController'
+import {signup,login,logout, verifyOTP, resendOTP} from '../Controllers/LoginController'
 
 router.post('/signup',signup)
 router.post('/login',login)
 router.post('/logout',logout)
 router.post('/verifyotp',verifyOTP)
+router.post('/resendotp',resendOTP)
 export default router;
